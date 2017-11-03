e259b70942ed944024288ed427cf18d9a3b46f90
LeonelMenendez <leo.dr@live.com.ar>
2017-11-03 00:09:06 -0300

    Se agrega el launcher

---
+++ Gestor de Libros/src/gestor/Launcher.java
@@ -0,0 +1,33 @@
+package gestor;
+
+import java.awt.EventQueue;
+
+import javax.swing.UIManager;
+import javax.swing.UIManager.LookAndFeelInfo;
+
+import login.Login;
+
+public class Launcher {
+	
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					for (LookAndFeelInfo info : UIManager.getInstalledLookAndFeels()) {
+						if ("Nimbus".equals(info.getName())) {
+							UIManager.setLookAndFeel(info.getClassName());
+							break;
+						}
+					}
+					Login frame = new Login();
+					frame.setLocationRelativeTo(null);
+					frame.setTitle("Gestor de Libros - Iniciar Sesión");
+					frame.setResizable(false);
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+}
